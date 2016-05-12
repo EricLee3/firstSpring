4cfe15feb58debb5e98719a2bcd28aa03d8e964d
EricLee3 <in.lee@isecommerce.co.kr>
2016-05-12 15:14:39 +0900

    testing branch commit

--- src/main/java/com/eric/first/HomeController.java
+++ src/main/java/com/eric/first/HomeController.java
@@ -32,6 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		// testing branch first 
 
 		return "home";
 	}
